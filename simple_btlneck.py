f702a8ec60750bf21d47a350ed146f46a02553f0
Lam <15622383059@163.com>
2019-07-23 17:27:35 +1000

    my simple linear topo

---
+++ simple_btlneck.py
@@ -0,0 +1,124 @@
+"""router topology example for TCP competions.
+   This remains the default version
+
+router between two subnets:
+
+   h1----+
+         |
+         r ---- h3
+         |
+   h2----+
+
+For running a TCP competition, consider the runcompetition.sh script
+"""
+
+QUEUE=1000
+DELAY='110ms'		# r--h3 link
+BottleneckBW=4
+BBR=False
+
+# reno-bbr parameters:
+if BBR:
+    DELAY='40ms'	
+    QUEUE=267
+    # QUEUE=25
+    QUEUE=10
+    BottleneckBW=10
+
+from mininet.net import Mininet
+from mininet.node import Node, OVSKernelSwitch, Controller, RemoteController
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.topo import Topo
+from mininet.log import setLogLevel, info
+
+# h1addr = '10.0.1.2/24'
+# h2addr = '10.0.2.2/24'
+# r1addr1= '10.0.1.1/24'
+# r1addr2= '10.0.2.1/24'
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        info ('enabling forwarding on ', self)
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
+
+class RTopo(Topo):
+    #def __init__(self, **kwargs):
+    #global r
+    def build(self, **_opts):     # special names?
+        defaultIP1 = '10.0.1.1/24'  # IP address for r0-eth1
+        defaultIP2 = '10.0.1.2/24'  # IP address for r0-eth1
+        # r1 = self.addNode( 'r1', cls=LinuxRouter, ip=defaultIP1 )
+        # r2 = self.addNode( 'r2', cls=LinuxRouter, ip=defaultIP2 )
+        r1 = self.addSwitch('r1')
+        r2 = self.addSwitch('r2')
+        h1 = self.addHost( 'h1', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1' )
+        h2 = self.addHost( 'h2', ip='10.0.1.11/24', defaultRoute='via 10.0.1.1' )
+
+        #  h1---80Mbit---r1---8Mbit/100ms---r2---80Mbit---h2
+ 
+        self.addLink( h1, r1, intfName1 = 'h1-eth', intfName2 = 'r1-eth1', bw=80,
+                 params2 = {'ip' : '10.0.1.1/24'})
+
+        self.addLink( h2, r2, intfName1 = 'h2-eth', intfName2 = 'r2-eth1',
+                 params2 = {'ip' : '10.0.1.2/24'})
+
+        self.addLink(r1, r2, intfName1='r1-eth2', intfName2='r2-eth2',
+                     bw=BottleneckBW, delay=DELAY, queue=QUEUE)
+
+# delay is the ONE-WAY delay, and is applied only to traffic departing h3-eth.
+
+# BBW=8: 1 KB/ms, for 1K packets; 110 KB in transit
+# BBW=10: 1.25 KB/ms, or 50 KB in transit if the delay is 40 ms.
+# queue = 267: extra 400 KB in transit, or 8x bandwidthxdelay
+
+def main():
+    rtopo = RTopo()
+    net = Mininet(topo = rtopo,
+                  link=TCLink,
+                  #switch = OVSKernelSwitch, 
+                  #controller = RemoteController,
+                  autoSetMacs = True   # --mac
+                  )
+    net.start()
+    r1 = net['r1']
+    r2 = net['r2']
+    # r.cmd('ip route list')
+    # r1.cmd('ifconfig r1-eth1 10.0.1.1/24')
+    # r2.cmd('ifconfig r2-eth1 10.0.1.2/24')
+    # r1.cmd('sysctl net.ipv4.ip_forward=1')
+    # r2.cmd('sysctl net.ipv4.ip_forward=1')
+    r2.cmd('tc qdisc change dev r2-eth2 handle 10: netem limit {}'.format(QUEUE))
+
+    h1 = net['h1']
+    h2 = net['h2']
+
+    h1.cmd('tc qdisc del dev h1-eth root')
+    h1.cmd('tc qdisc add dev h1-eth root fq')
+    h2.cmd('tc qdisc del dev h2-eth root')
+    h2.cmd('tc qdisc add dev h2-eth root fq')
+    for h in [r1, r2, h1, h2]: h.cmd('/usr/sbin/sshd')
+
+    CLI( net)
+    net.stop()
+
+setLogLevel('info')
+main()
+
+"""
+This leads to a queuing hierarchy on r with an htb node, 5:0, as the root qdisc. 
+The class below it is 5:1. Below that is a netem qdisc with handle 10:, with delay 110.0ms.
+We can change the limit (maximum queue capacity) with:
+
+	tc qdisc change dev r-eth1 handle 10: netem limit 5 delay 110.0ms
+
+"""
