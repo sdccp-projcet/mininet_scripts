e00b3a7ab05b1e0d3bb70f584507537768b84247
Lam <15622383059@163.com>
2019-08-19 20:32:58 +1000

    append output to a single file

--- plot_rtt_stat.py
+++ plot_rtt_stat.py
@@ -10,16 +10,15 @@ log_file_dir = "/home/lam/Projects/sdccp/FCA/build/"
 output_file = build_dir + "formatted_log"
 
 
-def plot_figure(f, out):
-    if os.path.exists(out):
-        os.remove(out)
+def plot_figure(f, out, interval):
     data = open(f).readlines()
     data = [x.split() for x in data]
 
     rtt = map(lambda d: float(d[4]), data)
     rtt_avg = numpy.average(rtt)
     rtt_std = numpy.std(rtt)
-    open(out, 'a').write('RTT avg.:{}, RTT std.:{}'.format(rtt_avg, rtt_std))
+    open(out, 'a+').write('interval: {}, RTT avg.:{}, RTT std.:{}\n'
+                          .format(interval, rtt_avg, rtt_std))
 
 
 parser = argparse.ArgumentParser()
@@ -38,6 +37,6 @@ if __name__ == '__main__':
     log_files = [f for f in listdir(log_file_dir) if isfile(join(log_file_dir, f))]
     log_files = map(lambda x: log_file_dir+x, log_files)
     for i, f in enumerate(log_files):
-        intermediate_file = output_file + str(i)
+        intermediate_file = output_file + "_" + str(i)
         format_file(f, intermediate_file)
-        plot_figure(intermediate_file, build_dir+'rtt_stats'+'_'+args.expr)
+        plot_figure(intermediate_file, build_dir+'rtt_stats', args.expr)
