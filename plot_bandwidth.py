2e964d8b7fc861df05713e16a67e39c3be0ec7b4
Lam <15622383059@163.com>
2019-08-28 11:12:21 +1000

    plot bandwidth

---
+++ plot_bandwidth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# import argparse
+import matplotlib.pyplot as plt
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--expr', '-e',
+#                     help="Experiment's name",
+#                     action="store",
+#                     required=True,
+#                     dest="expr")
+
+LOG_FILE = '/home/lam/Projects/sdccp/ryu/build/log.txt'
+
+
+def main():
+    data = open(LOG_FILE).readlines()
+    data = [d.split() for d in data]
+    data = filter(lambda x: x[1][0] != '0', data)
+    data = map(lambda x: [float(k) for k in x], data)
+    start_time = data[0][0]
+    x = [d[0] for d in data]
+    y = [d[1] for d in data]
+    x = map(lambda t: t - start_time, x)
+    y = map(lambda s: s / 1000000, y)
+
+    plt.figure()
+
+    plt.plot(x, y)
+    plt.xlabel('time(s)')
+    plt.ylabel('sending rate (Mbps)')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
